db6dcec125225ca41e44f649536633cebb695ad3
oumatoulacen <oumatoulacen@gmail.com>
2024-01-28 22:57:51 +0100

    Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+'''helper function file'''
+
+def index_range(page, page_size):
+    '''takes two integer arguments page and page_size'''
+    first_index = (page - 1) * page_size
+    last_index = first_index + page_size
+    return (first_index, last_index)
