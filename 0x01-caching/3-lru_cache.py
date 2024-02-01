2098ddf806464fa5a03f29bc3e2978e80944a5f8
oumatoulacen <oumatoulacen@gmail.com>
2024-02-01 02:11:04 +0100

    LRU caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,1 +1,32 @@
 #!/usr/bin/env python3
+''' implements LRU caching'''
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    ''' inherits from BaseCaching and is a caching system'''
+
+    def __init__(self):
+        ''' Initialize class instance. '''
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        ''' Assign the item to key of a dictionary '''
+        if key and item:
+            if key in self.cache_data:
+                self.order.remove(key)
+            elif len(self.cache_data) >= self.MAX_ITEMS:
+                discard = self.order.pop(0)
+                del self.cache_data[discard]
+                print('DISCARD: {}'.format(discard))
+            self.order.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        ''' Return the value linked to a key '''
+        if key and key in self.cache_data:
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data[key]
+        return None
