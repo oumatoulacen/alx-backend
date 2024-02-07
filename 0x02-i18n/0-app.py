4fee0fb3a9e7695b2efc0b4284de4afaadfdfcd2
oumatoulacen <oumatoulacen@gmail.com>
2024-02-07 01:51:16 +0100

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,15 +5,18 @@ from flask import Flask, render_template
 from flask_cors import CORS
 from flask_babel import Babel  # type: ignore
 
+
 app = Flask(__name__)
 babel = Babel(app)
 app.url_map.strict_slashes = False
+
 
 @app.route('/')
 def index():
     ''' This is the index page.'''
     print('Hello World!')
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
