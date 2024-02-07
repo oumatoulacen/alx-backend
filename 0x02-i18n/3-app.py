ba3df41a1886f7dbbe4ebc42a3c778338482f2ae
oumatoulacen <oumatoulacen@gmail.com>
2024-02-07 03:31:05 +0100

    Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -19,6 +19,7 @@ def get_locale():
     ''' determines the best match supported language.'''
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 class Config:
     ''' This is the configuration for the app.'''
     DEBUG = False
