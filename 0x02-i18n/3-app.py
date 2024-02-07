ceb50b11c8a3737eebbd453bf8f16443137689d6
oumatoulacen <oumatoulacen@gmail.com>
2024-02-07 03:38:03 +0100

    Parametrize templates/update

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -29,7 +29,7 @@ class Config:
     ''' This is the configuration for the app.'''
     DEBUG = False
     LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'fr'
+    BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
