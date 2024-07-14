50968903e6e1a491cc93ec6b5abcab43ccf615c2
Oumatou <oumatoulacen@gmail.com>
2024-07-14 01:07:01 +0100

    chore: Update .gitignore and package.json

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -12,12 +12,20 @@ client.on('error', function (err) {
 });
 
 //set hash key-value in HolbertonSchools list
-client.hset('HolbertonSchools', 'Portland', '50', print);
-client.hset('HolbertonSchools', 'Seattle', '80', print);
-client.hset('HolbertonSchools', 'New York', '20', print);
-client.hset('HolbertonSchools', 'Bogota', '20', print);
-client.hset('HolbertonSchools', 'Cali', '40', print);
-client.hset('HolbertonSchools', 'Paris', '2', print);
+// client.hset('HolbertonSchools', 'Portland', '50', print);
+// client.hset('HolbertonSchools', 'Seattle', '80', print);
+// client.hset('HolbertonSchools', 'New York', '20', print);
+// client.hset('HolbertonSchools', 'Bogota', '20', print);
+// client.hset('HolbertonSchools', 'Cali', '40', print);
+// client.hset('HolbertonSchools', 'Paris', '2', print);
+
+client.hmset('HolbertonSchools',
+  'Portland', '50',
+  'Seattle', '80',
+  'New York', '20',
+  'Bogota', '20',
+  'Cali', '40',
+  'Paris', '2', print);
 
 // retrieve all elements stored in HolbertonSchools list
 client.hgetall('HolbertonSchools', function (error, result) {
